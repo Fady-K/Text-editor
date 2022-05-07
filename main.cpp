a8f91fa59cb8a24963f29eb7ab1b1a14cb86a8dc
fady_kamal <popfadykamal151617@gmail.com>
2022-05-07 12:49:00 +0200

    modified the  display_menu function

--- main.cpp
+++ main.cpp
@@ -10,17 +10,18 @@ int main()
     bool program_running = true;
     string choice;
 
-    
+    display_menu(choice, true);
     // while true
     while (program_running)
     {
         if (choice == "1")
         {
-            cout << "hello" << endl;
+            
 
         }
 
     }
+    display_menu(choice, program_running);
 
 
 }
