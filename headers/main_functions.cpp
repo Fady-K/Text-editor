a1d0b5638b9bfd9d1ea14671a5826a6625cab52f
fady_kamal <popfadykamal151617@gmail.com>
2022-05-16 23:17:47 +0200

    added the rest of functions smalles and largest

--- headers/main_functions.cpp
+++ headers/main_functions.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cstring>
 #include <filesystem>
 #include <cctype>
 
@@ -22,12 +23,12 @@ void display_menu(string &choice, bool first_time)
 }
 
 //___________________________
-// load file function
-void load_file(fstream &file_to_load_content_in)
+// load file function  (this function loads the file in read and append mode)
+string file_path; // declared it as global in order to use it again in another function (read-write)
+void load_file_read_append(fstream &file_to_load_content_in)
 {
-    // take the file path
+    // take the file path, store in predecalred local variable file_path
     cout << "Enter file name: ";
-    string file_path;
     getline(cin >> ws, file_path);
     file_path += ".txt";
 
@@ -52,14 +53,44 @@ void load_file(fstream &file_to_load_content_in)
     if (!file_to_load_content_in.is_open())
     {
         cout << "#################" << endl;
+        cout << "!! Can't open" << file_path << " !!" << endl;
+        cout << "#################" << endl;
+    }
+}
+
+//___________________________
+// load file function  (this function loads the file in read and write mode)
+void load_file_read_write(fstream &file_to_load_content_in)
+{
+
+    // open the file
+    file_to_load_content_in.open(::file_path, ios::in | ios::out);
+
+    // if the file isn't open
+    if (!file_to_load_content_in.is_open())
+    {
+        cout << "#################" << endl;
         cout << "!! Can't open " << file_path << " !!" << endl;
         cout << "#################" << endl;
     }
 }
 
-// _______Smallest_id_functions______________
+//___________________________
+// load file function  (this function loads the file write mode only)
+void load_file_write_only(fstream &file_to_load_content_in)
+{
 
-//____________Middle_id_functions____________
+    // open the file
+    file_to_load_content_in.open(::file_path, ios::out);
+
+    // if the file isn't open
+    if (!file_to_load_content_in.is_open())
+    {
+        cout << "#################" << endl;
+        cout << "!! Can't open " << file_path << " !!" << endl;
+        cout << "#################" << endl;
+    }
+}
 
 // return the pointer again to the begging of the file >> to make the file modifyable (readable) again
 void restore_pointer(fstream &file_to_restore_its_pointer)
@@ -67,6 +98,123 @@ void restore_pointer(fstream &file_to_restore_its_pointer)
     file_to_restore_its_pointer.clear();
     file_to_restore_its_pointer.seekg(0, file_to_restore_its_pointer.beg);
 }
+
+// _______Smallest_id_functions______________
+
+//___________________________________________
+// dipslay function to display the all the content of the file
+void display_the_content(fstream &sourceFile)
+{
+
+    // check if file is empty
+    cout << "#################" << endl;
+    string line;
+    while (getline(sourceFile, line))
+    {
+
+        cout << line << endl;
+    }
+
+    cout << "#################" << endl;
+
+    restore_pointer(sourceFile);
+}
+
+//___________________________________________
+// Empty file to erase all the contents of the file
+void Empty_the_file()
+{
+    fstream temp;
+    temp.open(file_path, ios::out);
+    if (temp.is_open())
+    {
+        temp << "";
+        cout << "#################" << endl;
+        cout << "!! Contents of file has been erased successfully !!" << endl;
+        cout << "#################" << endl;
+    }
+    // note: in writing mode it's a must to close the file so as the changes be saved
+    temp.close();
+}
+
+void add_text(fstream &sourceFile)
+{
+    // take text to append from the user
+    cout << "Text to append: ";
+
+    string line;
+    getline(cin >> ws, line);
+    sourceFile << line << endl;
+
+    cout << "#################" << endl;
+    cout << "!! Text has been added successfully !!" << endl;
+    cout << "#################" << endl;
+
+    restore_pointer(sourceFile);
+}
+
+//___________________________________________
+void encrypt_file(fstream &sourceFile)
+{
+    fstream temp;
+    string word;
+    vector<string> encrypted_words;
+    while (sourceFile >> word)
+    {
+        // encrypt tht word
+        for (auto &c : word)
+        {
+            c = char(int(c) + 1);
+        }
+        word += " ";
+        encrypted_words.push_back(word);
+    }
+    restore_pointer(sourceFile);
+
+    temp.open(file_path, ios::out);
+
+    for (int i = 0; i < encrypted_words.size(); ++i)
+    {
+        temp << encrypted_words[i];
+    }
+    temp.close();
+
+    cout << "#################" << endl;
+    cout << "!! File was encrypted succesfully !!" << endl;
+    cout << "#################" << endl;
+}
+
+//_____________________________________________
+void decrypt_file(fstream &sourceFile)
+{
+    fstream temp;
+    string word;
+    vector<string> decrypted_words;
+    while (sourceFile >> word)
+    {
+        // encrypt tht word
+        for (auto &c : word)
+        {
+            c = char(int(c) - 1);
+        }
+        word += " ";
+        decrypted_words.push_back(word);
+    }
+    restore_pointer(sourceFile);
+
+    temp.open(file_path, ios::out);
+
+    for (int i = 0; i < decrypted_words.size(); ++i)
+    {
+        temp << decrypted_words[i];
+    }
+    temp.close();
+    cout << "#################" << endl;
+    cout << "!! File was decrypted succesfully !!" << endl;
+    cout << "#################" << endl;
+}
+
+//____________Middle_id_functions____________
 
 void count_words(fstream &file_to_count_its_words)
 {
@@ -161,6 +309,7 @@ void search_for_aword_in_file(fstream &file)
                 cout << "!! " << word_to_search << " in the file !!" << endl;
                 cout << "#################" << endl;
                 infile = true;
+                break;
             }
         }
     }
@@ -182,7 +331,7 @@ void merge(fstream &file_to_fill)
 {
     // declare a varible of fstream data type to store data in it
     fstream file_to_be_merged;
-    load_file(file_to_be_merged);
+    load_file_read_append(file_to_be_merged);
 
     // significant assets
     string line;
@@ -226,4 +375,226 @@ void merge(fstream &file_to_fill)
 
     // return the pointer again to the begging of the file >> to make the file modifyable (readable) again
     restore_pointer(file_to_fill);
+}
+
+//____________Largest_id_functions____________
+
+//________________________
+// convert all the content of file into lowercase
+void convertToLower(fstream &sourceFile)
+{
+    vector<string> vect;
+    string ch;
+
+    while (sourceFile.is_open())
+    {
+        getline(sourceFile, ch);
+        vect.push_back(ch);
+        if (sourceFile.eof())
+        {
+            break;
+        }
+    }
+    restore_pointer(sourceFile);
+
+    for (int i = 0; i < vect.size(); i++)
+    {
+        for (int j = 0; j < vect[i].size(); j++)
+        {
+            vect[i][j] = tolower(vect[i][j]);
+        }
+    }
+
+    for (int i = 0; i < vect.size(); i++)
+    {
+        sourceFile << vect[i] << endl;
+    }
+
+    cout << "#################" << endl;
+    cout << "!! File was converted into LOWERCASE succesfully !!" << endl;
+    cout << "#################" << endl;
+
+    restore_pointer(sourceFile);
+}
+
+//_____________________________________
+// convert all the words of the file into uppercase
+void convertToUpper(fstream &sourceFile)
+{
+    vector<string> vect;
+    string ch;
+
+    while (sourceFile.is_open())
+    {
+        getline(sourceFile, ch);
+        vect.push_back(ch);
+        if (sourceFile.eof())
+        {
+            break;
+        }
+    }
+    restore_pointer(sourceFile);
+
+    for (int i = 0; i < vect.size(); i++)
+    {
+        for (int j = 0; j < vect[i].size(); j++)
+        {
+            vect[i][j] = toupper(vect[i][j]);
+        }
+    }
+
+    for (int i = 0; i < vect.size(); i++)
+    {
+        sourceFile << vect[i] << endl;
+    }
+    cout << "#################" << endl;
+    cout << "!! File was converted into UPPERCASE succesfully !!" << endl;
+    cout << "#################" << endl;
+
+    restore_pointer(sourceFile);
+}
+
+//_________________________________
+// capitalize each word of the file
+void convertFToUpper(fstream &sourceFile)
+{
+    vector<string> vect;
+    string ch;
+
+    while (sourceFile.is_open())
+    {
+        getline(sourceFile, ch);
+        vect.push_back(ch);
+        if (sourceFile.eof())
+        {
+            break;
+        }
+    }
+    restore_pointer(sourceFile);
+
+    for (int i = 0; i < vect.size(); i++)
+    {
+        vect[i][0] = toupper(vect[i][0]);
+    }
+
+    for (int i = 0; i < vect.size(); i++)
+    {
+        for (int j = 1; j < vect[i].size(); j++)
+        {
+            if (isspace(vect[i][j]))
+            {
+                continue;
+            }
+            else
+            {
+                vect[i][j] = tolower(vect[i][j]);
+            }
+        }
+    }
+
+    for (int i = 0; i < vect.size(); i++)
+    {
+        sourceFile << vect[i] << endl;
+    }
+    cout << "#################" << endl;
+    cout << "!! File was CAPITALIZED succesfully !!" << endl;
+    cout << "#################" << endl;
+
+    restore_pointer(sourceFile);
+}
+
+//___________________________
+// take a word from user, return how many times it exits in the file
+void count_n_of_word(fstream &sourceFile)
+{
+    int counter = 0;
+    string word;
+    vector<string> vect;
+    string ch;
+
+    while (sourceFile.is_open())
+    {
+        sourceFile >> ch;
+        vect.push_back(ch);
+        vect.push_back(" ");
+        if (sourceFile.eof())
+        {
+            break;
+        }
+    }
+    restore_pointer(sourceFile);
+    for (int i = 0; i < vect.size(); i++)
+    {
+        for (int j = 0; j < vect[i].size(); j++)
+        {
+            vect[i][j] = tolower(vect[i][j]);
+        }
+    }
+    cout << "#################" << endl;
+    cout << "Enter a word to search for: ";
+    cin >> word;
+    for (int i = 0; i < word.size(); i++)
+    {
+        word[i] = tolower(word[i]);
+    }
+
+    for (int i = 0; i < vect.size(); i++)
+    {
+        if (vect[i] == word)
+        {
+            counter += 1;
+        }
+    }
+    cout << "#################" << endl;
+    cout << "Count of " << word << ": " << counter << endl;
+    cout << "#################" << endl;
+}
+
+// save function to save the user file, in addition to rename the file feature
+void save()
+{
+    // ask the user if he want's to change the password
+    cout << "Change the file name before saving it?  (y/n): ";
+    string answer;
+    getline(cin >> ws, answer);
+
+    // for (auto &c : answer)
+    //     c = tolower(c);
+
+    while (answer != "y")
+    {
+        cout << "#################" << endl;
+        cout << "!! invalid !! " << endl;
+        cout << "#################" << endl;
+        cout << "Change the file name (y/n): ";
+        getline(cin >> ws, answer);
+    }
+
+    // check the user's answer
+    if ((answer == "y") || (answer == "Y"))
+    {
+        // take the new name from the user
+        cout << "New name: ";
+        string new_name;
+        cin >> new_name;
+
+        // add .txt to new_name
+        new_name += ".txt";
+
+        // rename the file
+        // note: c_str() function convert string into const char* array
+        rename(file_path.c_str(), new_name.c_str());
+
+        // print !! saving is done!
+        cout << "#################" << endl;
+        cout << "!! " << new_name << " has been saved successfully !!" << endl;
+        cout << "#################" << endl;
+        file_path = new_name;
+    }
+    else if ((answer == "n") || (answer == "N"))
+    {
+        cout << "#################" << endl;
+        cout << "!! " << file_path << " has been saved successfully !!" << endl;
+        cout << "#################" << endl;
+    }
 }
