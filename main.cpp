0af5e305a1dd83378e4d79b4457de4385a950a1b
fady_kamal <popfadykamal151617@gmail.com>
2022-05-16 23:15:29 +0200

    modified main file (source code)

--- main.cpp
+++ main.cpp
@@ -8,35 +8,75 @@ int main()
 {
     bool program_running = true;
     string choice;
-    fstream file;
-    load_file(file);
+    fstream file_read_app, file_read_write;
+    load_file_read_append(file_read_app);
+    load_file_read_write(file_read_write);
+
     display_menu(choice, true);
     // while true
     while (program_running)
     {
         if (choice == "1")
         {
+            add_text(file_read_app);
+        }
+        else if (choice == "2")
+        {
+            display_the_content(file_read_app);
+        }
+        else if (choice == "3")
+        {
+            Empty_the_file();
+        }
+        else if (choice == "4")
+        {
+            encrypt_file(file_read_write);
+        }
+        else if (choice == "5")
+        {
+            decrypt_file(file_read_write);
         }
         else if (choice == "6")
         {
-            merge(file);
+            merge(file_read_app);
         }
         else if (choice == "7")
         {
-            count_words(file);
+            count_words(file_read_app);
         }
         else if (choice == "8")
         {
-            count_chars(file);
+            count_chars(file_read_app);
         }
         else if (choice == "9")
         {
-            count_lines(file);
+            count_lines(file_read_app);
         }
         else if (choice == "10")
         {
-            search_for_aword_in_file(file);
+            search_for_aword_in_file(file_read_app);
         }
+        else if (choice == "11")
+        {
+            count_n_of_word(file_read_app);
+        }
+        else if (choice == "12")
+        {
+            convertToUpper(file_read_write);
+        }
+        else if (choice == "13")
+        {
+            convertToLower(file_read_write);
+        }
+        else if (choice == "14")
+        {
+            convertFToUpper(file_read_write);
+        }
+        else if (choice == "15")
+        {
+            save();
+        }
+
         else if (choice == "16")
         {
             program_running = false;
